b415aefe36cd36272161e575db7d8c08be2afcb7
jordan <41rjordan@proton.me>
2024-09-25 01:52:01 +0500

    fix pics1

--- theme.config.jsx
+++ theme.config.jsx
@@ -21,7 +21,7 @@ const DynamicLogo = () => {
   return (
     <span style={{ display: 'flex', alignItems: 'center' }}>
       <Image 
-        src={currentTheme === 'dark' ? '/logo/white.png' : '/logo/black.png'} 
+        src={currentTheme === 'dark' ? '/public/logo/white.png' : '/public/logo/black.png'} 
         alt="Untron Logo" 
         width={128} 
         height={128} 
